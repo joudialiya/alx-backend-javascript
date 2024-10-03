fd35ade6f531bc57133a82eec8b2f4d35bc08d58
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-03 19:29:24 +0000

    10.

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   let newArray = [];
   for (const element of array) {
-    newArray = newArray.push(element + appendString);
+    newArray = newArray.push(appendString + element);
   }
 
   return newArray;
