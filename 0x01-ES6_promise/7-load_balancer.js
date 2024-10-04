33a63ce5c70374238a8bcb6ffb2eed0e66505bf7
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-04 14:30:29 +0000

    2-7-9

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,8 +1,4 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  let result;
-  Promise.any([chinaDownload, USDownload])
-    .then((value) => {
-      result = value;
-    });
-  return result;
+  return Promise.race([chinaDownload, USDownload])
+    .then((value) => value);
 }
--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -4,7 +4,7 @@ export default function guardrail(mathFunction) {
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    queue.push(error);
+    queue.push(String(error));
   } finally {
     queue.push('Guardrail was processed');
   }
