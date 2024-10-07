73c7e7abd1894760a54c552f3eda3b9ee7bb9942
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-07 23:21:26 +0000

    5. we nned to return a DataView object not an ArrayBuffer

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
   const view = new Int8Array(length);
-  if ((position >= length)) {
+  if (position >= length) {
     throw new Error('Position outside range');
   }
   view[position] = value;
-  return view.buffer;
+  return new DataView(view.buffer);
 }
