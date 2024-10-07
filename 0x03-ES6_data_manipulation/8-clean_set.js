eccd9d21f43342ecac93b3288901853e8576f172
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-07 23:27:09 +0000

    8.

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
   return Array.from(set).filter((e) => e.slice(0, startString.length) === startString)
     .map((e) => e.slice(startString.length))
     .join('-');
