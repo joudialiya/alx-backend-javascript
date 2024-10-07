304ee06c99eea895dc68c6a2ffb7c21e61498b4a
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-07 23:12:13 +0000

    5.

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   const view = new Int8Array(length);
-  if ((position < 0) || (position >= length)) {
+  if ((position >= length)) {
     throw new Error('Position outside range');
   }
   view[position] = value;
