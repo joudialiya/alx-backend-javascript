b977fed487239db7e235bc6baed9b4c9177d64da
Lagzal <mlagzal.sayhi@gmail.com>
2024-11-26 17:13:47 +0000

    4.

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const PORT = 1245;
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+server.listen(PORT);
