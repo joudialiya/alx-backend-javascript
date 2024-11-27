3cdbdc5f6ebadf64002f0c6408d573e44c108fba
Lagzal <mlagzal.sayhi@gmail.com>
2024-11-27 16:41:00 +0000

    7.

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const fs = require('express');
+
+const DB_FILE = process.argv[2] ? process.argv[2] : '';
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err || !data) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      const majorStudentsMap = {};
+      const lines = data.trim().split('\n').slice(1);
+      let res = '';
+      for (const line of lines) {
+        const firstname = line.split(',')[0];
+        const major = line.split(',')[3];
+        if (majorStudentsMap[major]) {
+          majorStudentsMap[major].push(firstname);
+        } else {
+          majorStudentsMap[major] = [firstname];
+        }
+      }
+      res += `Number of students: ${lines.length}\n`;
+      const majors = Object.getOwnPropertyNames(majorStudentsMap);
+      for (const major of majors) {
+        res += `Number of students in ${major}: ${majorStudentsMap[major].length}. List: ${majorStudentsMap[major].join(', ')}\n`;
+      }
+      resolve(res);
+    });
+  });
+}
+const app = express();
+const PORT = 1245;
+app.get('/', (_, res) => res.end('Hello Holberton School!'));
+app.get('/students', (_, res) => {
+  countStudents(DB_FILE)
+    .then((value) => res.end(`This is the list of our students\n${value}`))
+    .catch((err) => res.end(`This is the list of our students\n${err.toString()}`));
+});
+app.listen(PORT);
+module.exports = app;
