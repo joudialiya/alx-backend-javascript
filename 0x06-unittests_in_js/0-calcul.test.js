f3657cb16761fe055c442e3ff672bce6b6a192c7
Lagzal <mlagzal.sayhi@gmail.com>
2024-11-28 18:45:05 +0000

    0.

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('Test calculateNumber', () => {
+  it('1' , () => {
+    assert.equal(calculateNumber(1, 3), 4);
+  });
+  it('2' , () => {
+    assert.equal(calculateNumber(1, 3.7), 5);
+  });
+  it('3' , () => {
+    assert.equal(calculateNumber(1.2, 3.7),5);
+  });
+});
