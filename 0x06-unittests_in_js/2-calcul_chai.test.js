8f85a2ac304f390374a1e94f7f19636a76e691e2
Lagzal <mlagzal.sayhi@gmail.com>
2024-11-28 19:19:01 +0000

    2.

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -3,36 +3,36 @@ const calculateNumber = require('./1-calcul');
 
 describe('SUM', () => {
   it('1' , () => {
-    expect(calculateNumber('SUM', 1, 1)).equals(2);
+    expect(calculateNumber('SUM', 1, 1)).to.equal(2);
   });
   it('2' , () => {
-    expect(calculateNumber('SUM', 1, 3.7)).equals(5);
+    expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
   });
   it('3' , () => {
-    expect(calculateNumber('SUM', 1.2, 3.7)).equals(5);
+    expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
   });
 });
 
 describe('SUBTRACT', () => {
   it('1' , () => {
-    expect(calculateNumber('SUBTRACT', 1, 3)).equals(-2);
+    expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
   });
   it('2' , () => {
-    expect(calculateNumber('SUBTRACT', 6, 3.7)).equals(2);
+    expect(calculateNumber('SUBTRACT', 6, 3.7)).to.equal(2);
   });
   it('3' , () => {
-    expect(calculateNumber('SUBTRACT', 3, 3)).equals(0);
+    expect(calculateNumber('SUBTRACT', 3, 3)).to.equal(0);
   });
 });
 
 describe('DIVIDE', () => {
   it('1' , () => {
-    expect(calculateNumber('DIVIDE', -4, 1.2)).equals(-4);
+    expect(calculateNumber('DIVIDE', -4, 1.2)).to.equal(-4);
   });
   it('2' , () => {
-    expect(calculateNumber('DIVIDE', 6, 1.6)).equals(3);
+    expect(calculateNumber('DIVIDE', 6, 1.6)).to.equal(3);
   });
   it('3' , () => {
-    expect(calculateNumber('DIVIDE', 1.2, 0)).equals('Error');
+    expect(calculateNumber('DIVIDE', 1.2, 0)).to.equal('Error');
   });
 });
