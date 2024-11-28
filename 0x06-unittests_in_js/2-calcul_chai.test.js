bfaa8194f8c3d3c40d004baf91d556e82b8e089a
Lagzal <mlagzal.sayhi@gmail.com>
2024-11-28 19:52:58 +0000

    2.

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,36 +1,38 @@
-const chai = require('chai');
+const { expect } = require('chai');
 const calculateNumber = require('./1-calcul');
 
-const expect = chai.expect
-describe("SUM", function() {
-  it("1", function() {
-      expect(calculateNumber("SUM", 1, 2)).to.equal(3);
+describe('SUM', () => {
+  it('1' , () => {
+    expect(calculateNumber('SUM', 1, 1)).to.equal(2);
   });
-  it("2", function() {
-      expect(calculateNumber("SUM", 1.6, 2.6)).to.equal(5);
+  it('2' , () => {
+    expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
   });
-});
-
-describe("SUBTRACT", function() {
-  it("1", function() {
-      expect(calculateNumber("SUBTRACT", 1.4, 2.3)).to.equal(-1);
-  });
-  it("2", function() {
-      expect(calculateNumber("SUBTRACT", 4.9, 2.7)).to.equal(2);
-  });
-  it("3", function() {
-      expect(calculateNumber("SUBTRACT", -4.9, -2.7)).to.equal(-2);
+  it('3' , () => {
+    expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
   });
 });
 
-describe("DIVIDE", function() {
-  it("1", function() {
-      expect(calculateNumber("DIVIDE", 4, 2)).to.equal(2);
+describe('SUBTRACT', () => {
+  it('1' , () => {
+    expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
   });
-  it("2", function() {
-      expect(calculateNumber("DIVIDE", 4.6, 1.8)).to.equal(2.5);
+  it('2' , () => {
+    expect(calculateNumber('SUBTRACT', 6, 3.7)).to.equal(2);
   });
-  it("3", function() {
-      expect(calculateNumber("DIVIDE", 4, 0)).to.equal("Error");
+  it('3' , () => {
+    expect(calculateNumber('SUBTRACT', 3, 3)).to.equal(0);
+  });
+});
+
+describe('DIVIDE', () => {
+  it('1' , () => {
+    expect(calculateNumber('DIVIDE', -4, 1.2)).to.equal(-4);
+  });
+  it('2' , () => {
+    expect(calculateNumber('DIVIDE', 6, 1.6)).to.equal(3);
+  });
+  it('3' , () => {
+    expect(calculateNumber('DIVIDE', 1.2, 0)).to.equal('Error');
   });
 });
