2934d5263c9906a7184a9868679ff2b1899f9222
Lagzal <mlagzal.sayhi@gmail.com>
2024-11-29 02:15:47 +0000

    9.

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -15,7 +15,7 @@ describe('API integration test', () => {
   it('GET /cart/:id returns correct response for valid :id', (done) => {
     request.get(`${API_URL}/cart/1`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
-      expect(body).to.be.equal('Payment methods for cart 47');
+      expect(body).to.be.equal('Payment methods for cart 1');
       done();
     });
   });
