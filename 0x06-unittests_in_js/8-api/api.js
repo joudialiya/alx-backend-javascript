ae5dcaa143bfe6c0f954ab69b3b012fdc9d1f643
Lagzal <mlagzal.sayhi@gmail.com>
2024-11-29 01:59:26 +0000

    8.

---
+++ 0x06-unittests_in_js/8-api/api.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = 7865;
+
+app.get('/', (_, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.listen(PORT);
+
+module.exports = app;
